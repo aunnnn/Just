22e21f03d6688696ce7a2524f5c6ffbbf9a3f7d8
aunnnn <aun.wirawit@gmail.com>
2018-03-04 01:09:47 +0700

    Support iOS 8+, add macOS target

---
+++ Just-macOS/Just_macOS.h
@@ -0,0 +1,19 @@
+//
+//  Just_macOS.h
+//  Just-macOS
+//
+//  Created by Wirawit Rueopas on 3/4/2561 BE.
+//  Copyright © 2561 Wirawit Rueopas. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Just_macOS.
+FOUNDATION_EXPORT double Just_macOSVersionNumber;
+
+//! Project version string for Just_macOS.
+FOUNDATION_EXPORT const unsigned char Just_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Just_macOS/PublicHeader.h>
+
+
