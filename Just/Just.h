8eed75ed41f45417aa186a5466e1401081ac1aef
aunnnn <aun.wirawit@gmail.com>
2018-03-04 17:35:55 +0700

    House-cleaning

---
+++ Just/Just.h
@@ -0,0 +1,15 @@
+//
+//  Just.h
+//  Just
+//
+//  Created by Wirawit Rueopas on 3/4/2561 BE.
+//  Copyright © 2561 Wirawit Rueopas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/// Project version number for Just.
+extern double JustVersionNumber;
+
+/// Project version string for Just.
+extern const unsigned char JustVersionString[];
