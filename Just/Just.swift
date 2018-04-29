79d0d73e31f77c2004b2baee85fc69707ae7c71e
Wirawit Rueopas <aun.wirawit@gmail.com>
2018-04-29 15:30:57 +0700

    Remove request with urlString as it's redundant

--- Just/Just.swift
+++ Just/Just.swift
@@ -15,10 +15,4 @@ public struct Just {
     public static func request(_ url: URL, method: HTTPMethod, parameters: Parameters?=nil, headers: HTTPHeaders?=nil, configurationBlock: URLRequestConfigurationBlock?=nil) -> Request {
         return Request(url: url, method: method, parameters: parameters, headers: headers, configurationBlock: configurationBlock)
     }
-    
-    /// Make a request. Note that `method` supports only `.get` and `.post(POSTBodyEncoding)`, where POSTBodyEncoding is either `url` or `json`
-    public static func request(_ urlString: String, method: HTTPMethod, parameters: Parameters?=nil, headers: HTTPHeaders?=nil, configurationBlock: URLRequestConfigurationBlock?=nil) -> Request? {
-        guard let url = URL(string: urlString) else { return nil }
-        return Request(url: url, method: method, parameters: parameters, headers: headers, configurationBlock: configurationBlock)
-    }
 }
